5f19be33f7c07dcdab594ba9194855242c3cfaf3
Matheus <matheusmoraes199@gmail.com>
2021-04-18 01:30:22 -0300

    Primeiro arquivo

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int ret = 0;
+
+  printf("papobinario.com.br\n");
+  printf("%c", 10);
+  printf("%x - %x - %x - %c\n", ret, 10, 0xa, 0x41);
+  return 0;
+}
