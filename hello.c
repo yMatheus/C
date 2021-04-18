780d098bd034866699b6c94fb5d32b61f8a9d5b7
Matheus <matheusmoraes199@gmail.com>
2021-04-18 01:45:15 -0300

    Teste modificação do arquivo

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 int main(void) {
   int ret = 0;
 
-  printf("papobinario.com.br\n");
+  printf("Promovam o Zé pra gerente\n");
   printf("%c", 10);
   printf("%x - %x - %x - %c\n", ret, 10, 0xa, 0x41);
   return 0;
